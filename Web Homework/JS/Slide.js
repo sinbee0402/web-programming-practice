13c6f2ac5151a6a84af8c28695d294b934b04ab3
sinbee0402 <osb3476@gmail.com>
2020-11-18 20:20:03 +0900

    [Update] Slide page HTML img id and JS img1,2,3 change img

--- Web Homework/JS/Slide.js
+++ Web Homework/JS/Slide.js
@@ -1,11 +1,11 @@
 function button1(){
-    document.getElementById("img1").src="../images/Slide1.jpg";
+    document.getElementById("img").src="../images/Slide1.jpg";
 }
 
 function button2(){
-    document.getElementById("img2").src="../images/Slide3.jpg";
+    document.getElementById("img").src="../images/Slide3.jpg";
 }
 
 function button3(){
-    document.getElementById("img3").src="../images/Slide4.jpg";
+    document.getElementById("img").src="../images/Slide4.jpg";
 }
