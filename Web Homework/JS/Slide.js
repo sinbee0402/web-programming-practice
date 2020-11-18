575376b6111664a7756e63f6505920468723f662
sinbee0402 <osb3476@gmail.com>
2020-11-18 20:17:19 +0900

    [ADD] Slide page JS

---
+++ Web Homework/JS/Slide.js
@@ -0,0 +1,11 @@
+function button1(){
+    document.getElementById("img1").src="../images/Slide1.jpg";
+}
+
+function button2(){
+    document.getElementById("img2").src="../images/Slide3.jpg";
+}
+
+function button3(){
+    document.getElementById("img3").src="../images/Slide4.jpg";
+}
